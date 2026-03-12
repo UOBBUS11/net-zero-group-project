1849f178c521c30bb3932e1fd02805d8a6121dad
Huang Hsin Han <hans702197@hotmail.com>
2026-03-12 21:44:26 +0000

    Merge and test sprint1 function

--- app/models.py
+++ app/models.py
@@ -1,48 +1,36 @@
 from app import db
 from datetime import datetime
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))  # 密碼
     current_score = db.Column(db.Integer, default=0)
-    trips = db.relationship('Trip', backref='user', lazy=True)
+
+    trips = db.relationship('Trip', backref='user', lazy='dynamic')
+
 
 class TransportMode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    mode_name = db.Column(db.String(50), nullable=False)
-    emission_factor = db.Column(db.Float, nullable=False)
-    base_points = db.Column(db.Integer, nullable=False)
-    points_per_km = db.Column(db.Integer, nullable=False)
+    mode_name = db.Column(db.String(50))
+    emission_factor = db.Column(db.Float)
+    base_points = db.Column(db.Integer)
+    points_per_km = db.Column(db.Integer)
+
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    distance_km = db.Column(db.Float, nullable=False)
+    distance_km = db.Column(db.Float)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    carbon_emission = db.Column(db.Float, nullable=False)
-    score_earned = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    mode_id = db.Column(db.Integer, db.ForeignKey('transport_mode.id'), nullable=False)
+    carbon_emission = db.Column(db.Float)
+    score_earned = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    mode_id = db.Column(db.Integer, db.ForeignKey('transport_mode.id'))
     mode = db.relationship('TransportMode')
 
-<<<<<<< HEAD
-    def calculate_score(self):
 
-        self.carbon_emission = self.distance_km * self.mode.emission_factor
-        self.score_earned = self.mode.base_points + int(self.distance_km * self.mode.points_per_km)
-
-
-        self.user.current_score += self.score_earned
-
-
-        if self.score_earned < 0:
-            return "High emission! Try taking the Bus or Walking next time."
-        else:
-            return "Great Job! Thank you for choosing green transport."
-
-=======
->>>>>>> origin/AdminPanel-byAlim
 class Administrator(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(128))
